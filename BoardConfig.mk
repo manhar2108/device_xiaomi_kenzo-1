450f34ca2ceea2afab6379b6cbe5605f5db17496
Adarsh-MR <adarshmr1998@gmail.com>
2016-07-30 20:31:18 +0000

    update tc, and Update Deps
    - pull in all required repo's on breakfast
    
    Signed-off-by: Adarsh-MR <adarshmr1998@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -62,7 +62,7 @@ TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
 # Kernel Toolchain
 KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-android-4.9-kernel/bin
 KERNEL_TOOLCHAIN_PREFIX := aarch64-linux-android-
-TARGET_GCC_VERSION_ARM64 := 4.9-kernel
+TARGET_GCC_VERSION_ARM64 := 5.x-kernel
 
 # Optimizations
 STRICT_ALIASING := false
