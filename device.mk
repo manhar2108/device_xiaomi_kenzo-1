29b670181691c1419f7ceb28264f367d057244a5
Adarsh-MR <adarshmr1998@gmail.com>
2016-09-18 12:07:45 +0000

    more fixes to get it built - comment out fsck.te - Enable ADB during boot - Add missing Vendorsetup.sh
    Signed-off-by: Adarsh-MR <adarshmr1998@gmail.com>

--- device.mk
+++ device.mk
@@ -33,9 +33,14 @@ TARGET_SCREEN_WIDTH := 1080
 
 $(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
 
+# ANT+
+PRODUCT_PACKAGES += \
+    AntHalService \
+    com.dsi.ant.antradio_library \
+    libantradio
+
 # Permissions
 PRODUCT_COPY_FILES += \
-    library/com.dsi.ant.antradio_library.xml:system/etc/permissions/com.dsi.ant.antradio_library.xml \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,2 @@
 add_lunch_combo cardinal_kenzo-userdebug
+add_lunch_combo cardinal_kenzo-eng
